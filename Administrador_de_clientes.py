2e231e330c2f7a6eab5a3dad2d8307d4379ff4b6
Jesus <jesus.suarez1@ulatina.net>
2023-04-12 17:51:21 -0600

    Primer Commit

---
+++ Administrador_de_clientes.py
@@ -0,0 +1,11 @@
+customers = ["Filip", "Miranda", "Carmen", "Gerardo", "Stephania"]
+free_counters = [11, 2, 14, 5, 9]
+# La funcion 'Sort' ordena los elementos de menor a mayor, también en orden alfabetico
+customers.sort()
+free_counters.sort()
+
+customer = customers[1]
+counter = free_counters[2]
+
+print(customer)
+print('Visit counter:', counter)
